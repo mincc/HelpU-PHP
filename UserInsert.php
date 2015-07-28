4d458525ee6b43df5dc7eb8792d46fe201b8bfad
mincc <bengsnail2002@yahoo.com>
2015-07-28 17:33:15 +0800

    PHP file

---
+++ UserInsert.php
@@ -0,0 +1,17 @@
+<?php
+	include 'config.php';
+	include 'opendb.php';
+
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "INSERT INTO User (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_close($statement);
+    
+    include 'closedb.php';
+?>
